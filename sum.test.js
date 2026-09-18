0155f3475e09c40d46ebbc91cd35e91cf027910a
Pranav Jha <your_email@example.com>
2026-09-18 17:46:21 +0530

    Add demo files

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+import sum from "./sum.js";
+describe("test for sum fxn",()=>{
+    test("adds 2+2 to equal to 4",()=>{
+        expect(sum(2,2)).toBe(4);
+    })
+    test("adds -5 + -5 to equal to -10",()=>{
+        expect(sum(-5,-5)).toBe(-10);
+    })
+})
